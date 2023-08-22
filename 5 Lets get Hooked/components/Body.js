5aacedee87bef15c28e190f22d830659996c13f2
Zeeshan Safdar <zeeshan7826@gmail.com>
2023-08-22 09:19:58 +0500

    use useState hook

--- 5 Lets get Hooked/components/Body.js
+++ 5 Lets get Hooked/components/Body.js
@@ -1,14 +1,26 @@
 import Search from "./Search";
 import RestaurantCard from "./RestaurantCard";
 import restaurants from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [restaurantsList, setRestaurantsList] = useState(restaurants);
   return (
     <div>
-      <Search />
+      <div>
+        <button
+          className="btn-filter-top-rated"
+          onClick={() => {
+            const filteredRes = restaurantsList.filter((res) => res.info.avgRating > 4);
+            setRestaurantsList(filteredRes);
+          }}
+        >
+          Filter Top Rated
+        </button>
+      </div>
 
       <div className="restaurants-container">
-        {restaurants.map((restaurant) => {
+        {restaurantsList.map((restaurant) => {
           const { id, name, cuisines, avgRating, costForTwo, cloudinaryImageId } = restaurant.info;
           return (
             <RestaurantCard
--- 5 Lets get Hooked/components/RestaurantCard.js
+++ 5 Lets get Hooked/components/RestaurantCard.js
@@ -6,8 +6,8 @@ const RestaurantCard = (props) => {
       <img src={CARD_IMAGE_BASE_URL + image} alt="card image" />
       <h4>{name}</h4>
       <h4>{cuisines.join(", ")}</h4>
-      <h4>{price}</h4>
-      <h4>{avgRating}</h4>
+      <h4>Price {price}</h4>
+      <h4>Stars {avgRating}</h4>
     </div>
   );
 };
