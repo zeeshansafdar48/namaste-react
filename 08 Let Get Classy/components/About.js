b16d4b04c9ab8825989686fcffb3a1d98c096b0b
Zeeshan Safdar <zeeshan7826@gmail.com>
2023-08-29 08:54:04 +0500

    working on class base comp

--- 08 Let Get Classy/components/About.js
+++ 08 Let Get Classy/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div>
       <h1>About Us</h1>
-      <User name={"Zeeshan Safdar from Function"} />
+      {/* <User name={"Zeeshan Safdar from Function"} /> */}
       <UserClass name={"Zeeshan Safdar from Class"} />
     </div>
   );
--- 08 Let Get Classy/components/UserClass.js
+++ 08 Let Get Classy/components/UserClass.js
@@ -3,13 +3,29 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0
+    };
   }
   render() {
+    const { name } = this.props;
+    const { count } = this.state;
     return (
       <div className="user-container">
-        <h1>{this.props.name}</h1>
+        <h1>{name}</h1>
         <h2>Lahore Pakistan</h2>
         <h3>zeeshan7826@gmail.com</h3>
+        <h3>Count: {count}</h3>
+        <button
+          onClick={() => {
+            this.setState({
+              count: count + 1
+            });
+          }}
+        >
+          Click Increment{" "}
+        </button>
       </div>
     );
   }
