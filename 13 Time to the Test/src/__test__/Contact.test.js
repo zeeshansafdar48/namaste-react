93cf41003375dea5be2eb9c2f93431a46153e727
Zeeshan Safdar <zeeshan7826@gmail.com>
2023-10-16 13:13:16 +0500

    it & describe method in test cases

--- 13 Time to the Test/src/__test__/Contact.test.js
+++ 13 Time to the Test/src/__test__/Contact.test.js
@@ -2,13 +2,26 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../components/Contact";
 import "@testing-library/jest-dom";
 
-test("should render Contact Us", () => {
-  // rendering
-  render(<Contact />);
+describe("Contact Us Page", () => {
+  it("should render Contact Us", () => {
+    // rendering
+    render(<Contact />);
 
-  // Querying
-  const heading = screen.getByRole("heading");
+    // Querying
+    const heading = screen.getByRole("heading");
 
-  // Assertion
-  expect(heading).toBeInTheDocument();
+    // Assertion
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("should check button on Contact Us", () => {
+    // rendering
+    render(<Contact />);
+
+    // Querying
+    const button = screen.getByRole("button");
+
+    // Assertion
+    expect(button).toBeInTheDocument();
+  });
 });
